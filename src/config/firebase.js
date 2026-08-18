254d69204917b3598694ba6eb5d952c6b95205a3
Shin Chan <shinchan8733@gmail.com>
2026-08-18 15:53:58 +0530

    fix database url typo

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -5,7 +5,7 @@ import { getDatabase } from 'firebase/database'
 const firebaseConfig = {
   apiKey: "AIzaSyB8M46YZwx37Z2d_Bs_uU6vX-9vpmW9IyQ",
   authDomain: "you-and-me-acd13.firebaseapp.com",
-  databaseURL: "https://you-and-me-acd13-default-rtdb.asia-southeast1.firebasedatabase.appp",
+  databaseURL: "https://you-and-me-acd13-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "you-and-me-acd13",
   storageBucket: "you-and-me-acd13.firebasestorage.app",
   messagingSenderId: "639146623095",
